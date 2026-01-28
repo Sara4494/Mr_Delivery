9117c8063a0c67d5f7a164df21b67919ee61649c
Sara4494 <riad52166@gmail.com>
2026-01-28 22:40:30 +0200

    chore: Refactor ASGI configuration for improved middleware import order
    - Move import statements for routing and middleware to follow Django setup.
    - Ensure that the ASGI application is properly configured before importing additional components.

--- mr_delivery/asgi.py
+++ mr_delivery/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-import shop.routing
-from shop.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mr_delivery.settings')
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+# الآن يمكن استيراد routing و middleware بعد setup Django
+from channels.routing import ProtocolTypeRouter, URLRouter
+import shop.routing
+from shop.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
