de10a2c21323b78ba314ddbb244116b87a2d60e3
Sara4494 <riad52166@gmail.com>
2026-01-28 19:15:18 +0200

    chore: Clean up settings.py by removing unnecessary whitespace
    - Remove trailing whitespace in INSTALLED_APPS section for better code readability.

--- mr_delivery/settings.py
+++ mr_delivery/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
-    'daphne',
-    
+ 
+
     'rest_framework',
     'rest_framework_simplejwt',
     'user',
